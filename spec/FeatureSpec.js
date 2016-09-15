8d100d4edd8f58589f2d61a628e434dc84b3d0c0
Alex Fakhri <alexfakhri@me.com>
2016-09-15 17:36:38 +0100

    Added takeoff feature

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -10,10 +10,17 @@ describe('Feature Test:', function() {
     airport = new Airport();
   });
 
-  it ('planes can be instructed to land at an airport', function() {
+  it('planes can be instructed to land at an airport', function() {
     plane.land(airport)
     expect(airport.planes()).toContain(plane);
   });
+
+  it('planes can be instructed to take off', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
 
 
 });
